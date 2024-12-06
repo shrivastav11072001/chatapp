151f6d76d930bc46bbabcc2562ae70042ebd2a43
Rahul Shrivastav <rahul.shrivastav.226169@gmail.com>
2024-12-06 17:45:37 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -22,34 +22,7 @@ app.use("/auth", authRoutes);
 app.use("/messages", messageRoutes);
 app.use("/users", userRoutes);
 
-// const server = createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:3000"],
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// const getReceiverSocketId = (receiverId)=>{
-//   return userSocketMap[receiverId];
-// }
-
-// const userSocketMap = {};
-
-// io.on("connection", (socket) => {
-//   console.log("A user is connected", socket.id);
-
-//   const userId = socket.handshake.query.userId;
-//   if (userId) {
-//     userSocketMap[userId] = socket.id
-//   }
-
-//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   socket.on("disconnect", () => {
-//     delete userSocketMap[userId];
-//     io.emit("getOnlineUser", Object.keys(userSocketMap));
-//   });
-// });
+// const server = createServer
 
 const PORT = process.env.PORT || 5000;
 server.listen(process.env.PORT, () => {
